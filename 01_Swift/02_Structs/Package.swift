92a4b791aef72075c5b341fdfe2aff464164cc22
Juana Paulina Aguila Hernandez <vegenisennawa@users.noreply.github.com>
2026-07-15 19:28:52 -0600

    Renombramiento de proyecto
    a) Se renombra el proyecto "Optionals" por "Structs".

--- 01_Swift/02_Structs/Package.swift
+++ 01_Swift/02_Structs/Package.swift
@@ -4,16 +4,16 @@
 import PackageDescription
 
 let package = Package(
-    name: "02_Optionals",
+    name: "02_Structs",
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
-            name: "02_Optionals"
+            name: "02_Structs"
         ),
         .testTarget(
-            name: "02_OptionalsTests",
-            dependencies: ["02_Optionals"]
+            name: "02_StructsTests",
+            dependencies: ["02_Structs"]
         ),
     ],
     swiftLanguageModes: [.v6]
