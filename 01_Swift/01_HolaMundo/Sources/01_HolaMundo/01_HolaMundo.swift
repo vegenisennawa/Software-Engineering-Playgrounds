24dae84d71c9bb7d1bfea4d8eb45dcbddf7fa31f
Juana Paulina Aguila Hernandez <vegenisennawa@users.noreply.github.com>
2026-07-16 17:30:07 -0600

    Muestra del primer ejercicio.
    Se realiza el primer ejercicio listado en el README:
    Declara tres constantes (`let`) para un nombre, una profesión y una ciudad. Imprime una sola línea en consola usando interpolación de cadenas: `"Hola, soy \(nombre), trabajo como \(profesion) en \(ciudad)."` con una variante.

--- 01_Swift/01_HolaMundo/Sources/01_HolaMundo/01_HolaMundo.swift
+++ 01_Swift/01_HolaMundo/Sources/01_HolaMundo/01_HolaMundo.swift
@@ -1,20 +1,42 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
+/*
+Multicomentario
+Ejercicio de dos partes:
+a) Tres variables let con nombre, edad y ciudad. Imprime un mensaje con interpolación de cadenas.
+b) Calculadora de 15% de propinas.
+*/
 @main
 struct HolaMundo {
     static func main() {
+        //Contador de ejercicios completados.
+        var ejerciciosCompletados = 0
+
+        //Primer ejercicio.
+        let nombre: String = "Paulina"
+        let edad: Int = 37
+        let profesion: String = "Desarrollador"
+
+        print("¡Hola! Mi nombre es \(nombre), tengo \(edad) y trabajo como \(profesion).")
+
+        ejerciciosCompletados += 1
+        print("¡Avanzando! Ejercicios listos: \(ejerciciosCompletados)")
+
+        /*
+        Primeros ejercicios de Swift, antes del 16-07-2024.
+        */
         //print("Hello, world!")
 
         // 1. Tipado inferido e inmutabilidad
-        let lenguaje: String = "Swift"
-        var ejerciciosCompletados = 0
+        //let lenguaje: String = "Swift"
+        //var ejerciciosCompletados = 0
 
-        print("Iniciando estudio de \(lenguaje). Ejercicios listos: \(ejerciciosCompletados)")
+        //print("Iniciando estudio de \(lenguaje). Ejercicios listos: \(ejerciciosCompletados)")
 
         // 2. Mutando el estado permitido
-        ejerciciosCompletados += 1
-        print("¡Avanzando! Ejercicios listos: \(ejerciciosCompletados)")
+        //ejerciciosCompletados += 1
+        //print("¡Avanzando! Ejercicios listos: \(ejerciciosCompletados)")
 
         // 3. LA PRUEBA DE FUEGO (Descomenta la línea de abajo, guarda y ejecuta `swift run` en tu terminal x64):
         // lenguaje = "C#"
